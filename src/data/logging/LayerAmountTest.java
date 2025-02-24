a976fdb1d29a4892ae14557e62db600b58b97b9a
Kai Benson <kaitaobenson@gmail.com>
2025-02-24 11:50:59 -0800

    wtf fix

--- src/data/logging/LayerAmountTest.java
+++ src/data/logging/LayerAmountTest.java
@@ -1,4 +1,4 @@
-package data;
+package data.logging;
 
 import activation.IActivationFunction;
 import network.NeuronLayer;
--- src/data/logging/PointLogger.java
+++ src/data/logging/PointLogger.java
@@ -1,4 +1,4 @@
-package data;
+package data.logging;
 
 import java.io.FileWriter;
 import java.io.IOException;
